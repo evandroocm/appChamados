d6e137ee97920ed7f406308fcd58a4d584173512
Evandro Marques <evandrocastromarques@gmail.com>
2025-06-25 17:11:04 -0400

    kiwi

--- appChamados/app/login/login.tsx
+++ appChamados/app/login/login.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 
 export default function Login() {
   return (
     <View className="flex-1 justify-center items-center">
-      <View style={styles.logoContainer}>
-
+      <Image style={styles.image} source={require('../../assets/images/background.png')}/>
+      <View style={styles.containerPai}>
+        <Image
+          source={require("../../assets/images/logo.png")}
+          style={{ width: 100, height: 100 }}
+        />
       </View>
       <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
     </View>
@@ -13,7 +17,6 @@ export default function Login() {
 }
 
 const styles = StyleSheet.create({
-  logoContainer: {
+  image: {
     
-  }
 });
